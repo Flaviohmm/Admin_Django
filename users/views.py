228b137ecc13ae95d897b1104c59300892593d94
Flaviohmm <fhenrique609@gmail.com>
2022-08-27 15:34:30 -0300

    Models

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import User
 
 
 @api_view(['GET'])
-def hello(request):
-    return Response('Hello')
+def users(request):
+    users = User.objects.all()
+    return Response(users)
